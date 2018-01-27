10b678869b15259110ca2039ee2d26b7d68ff6b9
Nick <nick.lewellen@gmail.com>
2018-01-26 22:47:14 -0600

    RS is done. Need AM notes on the buy - sell signal and other indicators.

--- techs.R
+++ techs.R
@@ -1,10 +1,36 @@
 library(tidyquant)
 library(quantmod)
 library(tidyverse)
+library(zoo)
 
 
+# This function calls an API for data and cleans the data
 
-ozrk <- as_tibble(getSymbols("OZRK", env = NULL))%>%
-            rownames_to_column()%>%
-            magrittr::set_colnames(c("Date","Open", "High", "Low", "Close", "Volume", "Adjusted"))%>%
-            mutate(Ticker = "OZRK")
+stock_call <- function(x){
+    df <- as_tibble(getSymbols(x, env = NULL))%>%
+        rownames_to_column()%>%
+        magrittr::set_colnames(c("Date","Open", "High", "Low", "Close", "Volume", "Adjusted"))%>%
+        mutate(Ticker = x)
+    
+    return(df)
+}
+
+# List of stock tickers -- to be appended by AM
+
+ticks <- c("OZRK", "IBM")
+
+# Create a dataframe from the list of tickers and stock_call
+
+stock_data <- as.data.frame(do.call(rbind,(lapply(ticks, stock_call))))
+
+# Make RS and RS_14
+
+stock_data <- stock_data%>%
+                group_by(Ticker)%>%
+                arrange(Ticker, Date)%>%
+                mutate(Change = Close -lag(Close, default = first(NA)), Gain = if_else(Change > 0, Change, 0),
+                       Loss = if_else(Change <= 0, abs(Change), 0), 
+                       Avg_Gain = rollapply(Gain, 14, mean, align='right',fill=NA),
+                       Avg_Loss = rollapply(Loss, 14, mean, align='right',fill=NA),
+                       RS = (Avg_Gain / Avg_Loss), RS_14 = if_else(Avg_Loss == 100, 100, 100 - (100 / (1 + RS))))
+ 
